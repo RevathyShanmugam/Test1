8596ba262dfa9c910d79d19855ee067987a4399c
rshanmugam <rshanmugam@LTRSHANMUGAM.genomichealth.com>
2020-07-19 15:08:43 -0700

    adding assignmments

---
+++ src/Assignments/Assignment1.java
@@ -0,0 +1,31 @@
+package Assignments;
+
+public class Assignment1 {
+
+	public static void main(String[] args) 
+	{
+		
+		//char b=85;
+		//char a='b';
+		//System.out.println(a+b);
+		
+		
+		//byte a=100;
+		//short b=20;
+		//int c= a+b;
+		//System.out.println(c);
+		
+		
+	//	int a =100;
+		//long b=20;
+		//long c=a+b;
+		//System.out.println(c);
+		
+		double a=5.0;
+		double b=15.2;
+		double c=a+b;
+		System.out.println(c);
+		
+	}
+
+}
---
+++ src/Assignments/Assignment1SeleniumOnlineTraining.java
@@ -0,0 +1,41 @@
+package Assignments;
+
+public class Assignment1SeleniumOnlineTraining {
+ 
+	String trainerName="Mukesh";
+		
+	public static void main(String[] args) 
+	{
+	Assignment1SeleniumOnlineTraining price1=new Assignment1SeleniumOnlineTraining();
+	price1.seleniumTraining();
+	//SeleniumOnlineTraining price2=new SeleniumOnlineTraining();
+	//price2.seleniumTraining();
+	//SeleniumOnlineTraining price3=new SeleniumOnlineTraining();
+	//price3.seleniumTraining();
+	}
+	
+	
+	public void seleniumTraining() 
+	{
+		//int price1=200;
+		System.out.println("Selenium Training charges are 200USD and trainer name is " + trainerName);
+		
+		
+	}
+	
+	public void devOpsTraining() 
+	{
+		//int price1=200;
+		System.out.println("DevOps Training charges are 300USD and trainer name is " + trainerName);	
+		
+		
+	}
+	
+	public void jmeterTraining() 
+	{
+		//int price1=200;
+		System.out.println("DevOps Training charges are 400USD and trainer name is " + trainerName);
+		
+		
+	}
+}
